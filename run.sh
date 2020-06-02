2d6de3854291e2fad4f9cffe581a0080210855f0
Ibraheem Alqwaiz <ialqwaiz@users.noreply.github.com>
2020-06-02 05:30:42 +0300

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 if ! pgrep -f 'subgrab'
 then
-    nohup subgrab -d $TARGET -l $LANG >> /tmp/log.txt 2>&1
+    nohup subgrab -d /media -l $LANG -c $COUNT >> /tmp/log.txt 2>&1
 else
     echo "running" >> ~/out_test.txt
 fi
